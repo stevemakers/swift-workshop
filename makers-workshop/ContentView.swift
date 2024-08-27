ddafb63d5d1c6c710250ebe08131a1b4263ac914
Steve A <steve.alex@makers.tech>
2024-08-27 13:15:12 +0100

    Initial Commit

---
+++ makers-workshop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  makers-workshop
+//
+//  Created by Steve Alex  on 27/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ makers-workshop/makers_workshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  makers_workshopApp.swift
+//  makers-workshop
+//
+//  Created by Steve Alex  on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct makers_workshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
